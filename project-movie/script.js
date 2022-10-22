811596b52c2a4389eed1bf987deab59627af08ee
TatianaYakushonok <yakush2@yandex.ru>
2022-10-22 17:12:09 +0700

    feat: add favorit genres (Sat, 22 Oct 2022 17:12:08 +0700)

--- project-movie/script.js
+++ project-movie/script.js
@@ -1,5 +1,15 @@
 import  './cycles.js';
-const number0fFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+let number0fFilms;
+
+function start() {
+    number0fFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (number0fFilms == '' || number0fFilms == null && isNaN(number0fFilms)) {
+        number0fFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: number0fFilms,
@@ -9,15 +19,19 @@ const personalMovieDB = {
     privat: false
 };
 
-if (personalMovieDB.count < 10) {
-    alert('Просмотрено довольно мало фильмов');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    alert('Вы классический зритель');
-} else if (personalMovieDB.count >= 30) {
-    alert('Вы киноман');
-} else (
-    alert('Произошла ошибка')
-);
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        alert('Просмотрено довольно мало фильмов');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        alert('Вы классический зритель');
+    } else if (personalMovieDB.count >= 30) {
+        alert('Вы киноман');
+    } else (
+        alert('Произошла ошибка')
+    ); 
+}
+
+detectPersonalLevel();
 
 /*for (let i = 1; i < 3; i++) {
     let movieFilm = prompt('Один из последних просмотренных фильмов?', '');
@@ -30,17 +44,35 @@ if (personalMovieDB.count < 10) {
     }
 };*/
 
-let count = 1;
-while (count < 3) {
-    let movieFilm = prompt('Один из последних просмотренных фильмов?', '');
-    let scoreFilm = prompt('На сколько оцените его?', '');
-    count++;
+function rememberMyFilms() {
+    let count = 1;
+    while (count < 3) {
+        let movieFilm = prompt('Один из последних просмотренных фильмов?', '');
+        let scoreFilm = prompt('На сколько оцените его?', '');
+        count++;
 
-    if(movieFilm != null && scoreFilm != null && movieFilm != '' && scoreFilm != '' && movieFilm.length < 50) {
-        personalMovieDB.movies[movieFilm] = scoreFilm;
-    } else {
-        count--;
+        if(movieFilm != null && scoreFilm != null && movieFilm != '' && scoreFilm != '' && movieFilm.length < 50) {
+            personalMovieDB.movies[movieFilm] = scoreFilm;
+        } else {
+            count--;
+        }
+    };
+}
+
+rememberMyFilms();
+
+function showMyDB() {
+    if(personalMovieDB.privat == false) {
+        console.log(personalMovieDB);
     }
 }
 
-console.log(personalMovieDB);
+showMyDB();
+
+function writeYourGenres() {
+    for(let i = 1; i <= 3; i++) {
+        personalMovieDB.genres.push(prompt(`Ваш любимый жанр под номером ${i}`));
+    }
+}
+
+writeYourGenres();
